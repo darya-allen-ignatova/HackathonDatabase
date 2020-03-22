45c5370c93e49931f221545862498de6fcac479f
Darya Ignatova <darya.ignatova.99@mail.ru>
2020-03-22 11:57:18 +0300

    Country and City storing tables

---
+++ EventDatabase/dbo/Tables/City.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[City] (
+    [Id]            INT               IDENTITY(1,1) NOT NULL,
+    [Name]          NVARCHAR (50)     NOT NULL,
+	[CountryId]     INT               NOT NULL,
+    CONSTRAINT [PK_City] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
