520c0f182dfe568f33916c5914f791baf695bd52
Darya Ignatova <darya.ignatova.99@mail.ru>
2020-03-22 11:58:11 +0300

    Main table for event storing

---
+++ EventDatabase/dbo/Tables/Event.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Event] (
+    [Id]                INT                IDENTITY(1,1) NOT NULL,
+    [Name]              NVARCHAR (50)     NOT NULL,
+    [StartDateTime]     DATE              NOT NULL,
+    [EndDateTime]       DATE              NOT NULL,
+	[Status]            INT               NOT NULL,
+    [DirectionId]       INT               NOT NULL,
+	[CityId]            INT               NOT NULL,
+	[LogoId]            INT               NOT NULL,
+    CONSTRAINT [PK_Event] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_Event_Direction] FOREIGN KEY ([DirectionId]) REFERENCES [dbo].[Direction] ([Id]),
+	CONSTRAINT [FK_Event_City] FOREIGN KEY ([CityId]) REFERENCES [dbo].[City] ([Id]),
+	CONSTRAINT [FK_Event_Logo] FOREIGN KEY ([LogoId]) REFERENCES [dbo].[File] ([Id])
+);
+
