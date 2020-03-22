5db22b161e0416ec6ae887b464c16c078aecbfc6
Darya Ignatova <darya.ignatova.99@mail.ru>
2020-03-22 11:57:51 +0300

    Storing different file types table

---
+++ EventDatabase/dbo/Tables/File.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[File] (
+    [Id]      INT                 IDENTITY(1,1) NOT NULL,
+    [Name]    NVARCHAR (100)      NOT NULL,
+	[Title]   NVARCHAR (50)       NOT NULL,
+    [Data]    VARBINARY (MAX)     NOT NULL,
+
+    CONSTRAINT [PK_File] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
