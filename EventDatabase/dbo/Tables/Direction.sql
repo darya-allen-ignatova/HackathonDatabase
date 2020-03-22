31904fc3d9fad41b1ae134064d4ac9247cbae18b
Darya Ignatova <darya.ignatova.99@mail.ru>
2020-03-22 11:59:38 +0300

    Direction of events table

---
+++ EventDatabase/dbo/Tables/Direction.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Direction] (
+    [Id]      INT               IDENTITY(1,1) NOT NULL,
+    [Name]    NVARCHAR (100)    NOT NULL,
+
+    CONSTRAINT [PK_Direction] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
