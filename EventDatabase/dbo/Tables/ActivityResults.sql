8bf8ecfbfede34a65312fef9c5897ff27e4bc772
Darya Ignatova <darya.ignatova.99@mail.ru>
2020-03-22 13:04:20 +0300

    Changes in tables according to idea that there are only group results

--- EventDatabase/dbo/Tables/ActivityResults.sql
+++ EventDatabase/dbo/Tables/ActivityResults.sql
@@ -1,13 +1,11 @@
 ﻿CREATE TABLE [dbo].[ActivityResults] (
     [Id]            INT      IDENTITY(1,1) NOT NULL,
     [ActivityId]    INT      NOT NULL,
-    [UserId]        INT      NULL,
-    [GroupId]       INT      NULL,
+    [GroupId]       INT      NOT NULL,
 	[Score]         REAL     DEFAULT(0) NOT NULL,
 
     CONSTRAINT [PK_ScoreResult] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_ScoreResult_Activity] FOREIGN KEY ([ActivityId]) REFERENCES [dbo].[Activity] ([Id]),
-    CONSTRAINT [FK_ScoreResult_Group] FOREIGN KEY ([GroupId]) REFERENCES [dbo].[Group] ([Id]),
-    CONSTRAINT [FK_ScoreResult_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id])
+    CONSTRAINT [FK_ScoreResult_Group] FOREIGN KEY ([GroupId]) REFERENCES [dbo].[Group] ([Id])
 );
 
--- EventDatabase/dbo/Tables/Task.sql
+++ EventDatabase/dbo/Tables/Task.sql
@@ -3,8 +3,10 @@
     [Name]          NVARCHAR (100)   NOT NULL,
     [Description]   NVARCHAR (MAX)   NOT NULL,
     [ActivityId]    INT              NOT NULL,
+    [GroupId]       INT              NOT NULL, 
 
     CONSTRAINT [PK_Task] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Task_Activity] FOREIGN KEY ([ActivityId]) REFERENCES [dbo].[Activity] ([Id])
+    CONSTRAINT [FK_Task_Activity] FOREIGN KEY ([ActivityId]) REFERENCES [dbo].[Activity] ([Id]),
+	CONSTRAINT [FK_Task_Group] FOREIGN KEY ([GroupId]) REFERENCES [dbo].[Group] ([Id])
 );
 
