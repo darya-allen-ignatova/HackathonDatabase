2c09388f3c9bee12be30e4387ef47f1a49c7e952
Darya Ignatova <darya.ignatova.99@mail.ru>
2020-03-22 12:43:45 +0300

    Task table for task

---
+++ EventDatabase/dbo/Tables/Task.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Task] (
+    [Id]            INT              IDENTITY(1,1) NOT NULL,
+    [Name]          NVARCHAR (100)   NOT NULL,
+    [Description]   NVARCHAR (MAX)   NOT NULL,
+    [ActivityId]    INT              NOT NULL,
+
+    CONSTRAINT [PK_Task] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Task_Activity] FOREIGN KEY ([ActivityId]) REFERENCES [dbo].[Activity] ([Id])
+);
+
